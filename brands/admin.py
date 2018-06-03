cbe3565e3cdec0e7dcd9af61d0345ed3bf0ad4da
smartworld1004 <smartworld1004@outlook.com>
2018-06-03 10:12:23 +0800

    fix: Add brands to users

--- brands/admin.py
+++ brands/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
 # Register your models here.
 from .models import Brand
 
-admin.site.register(Brand)
+class BrandInline(admin.StackedInline):
+    model = Brand
+    can_delete = False
+    verbose_name_plural = 'brand'
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (BrandInline, )
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- episoze/urls.py
+++ episoze/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('landing.urls')),
+    url(r'^', include('landing.urls')),
     url(r'^articles/', include('articles.urls')),
     url(r'^brands/', include('brands.urls')),
     url(r'^episodes/', include('episodes.urls')),
