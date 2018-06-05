4391c09a52458e589655ffc7467cdb8ae1edc999
smartworld1004 <smartworld1004@outlook.com>
2018-06-05 11:16:33 +0800

    change: href brnad.name => brand.name_slug

--- brands/urls.py
+++ brands/urls.py
@@ -4,5 +4,5 @@ from articles import views
 
 urlpatterns = [
 	path('', views.index_brand, name="index_brand"),
-    path('<slug:name>/', views.show_brand, name="show_brand"),
+    path('<slug:name_slug>/', views.show_brand, name="show_brand"),
 ]
